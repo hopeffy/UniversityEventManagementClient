5ff4b52c5de230327b1a6e9e0882b4ac2b1faa2e
Eftelya <95256134+hopeffy@users.noreply.github.com>
2024-01-23 18:09:38 +0300

    event card responsivity
    -event card resposivity
    -event card date picker

--- src/app/pages/events/events/events.component.ts
+++ src/app/pages/events/events/events.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../components/navbar/navbar/navbar.component';
 import { Events } from '../../../interfaces/Events';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css'
+  styleUrl: './events.component.css',
+  providers: [provideNativeDateAdapter()],
 })
 export class EventsComponent {
-  
+  date = new FormControl(new Date());
+
 
   constructor() {
     
